099d30a20fa02b99e62367ac9616ed8467933d36
Khoa Pham <kenjifam@gmail.com>
2019-01-18 19:39:37 +0200

    finished exercise w1/02/alt2/TODO 2

--- w1 JavaScript Language/02 JavaScript Language/alt2/js/docker.js
+++ w1 JavaScript Language/02 JavaScript Language/alt2/js/docker.js
@@ -1,3 +1,57 @@
+class Stuff {
+  constructor(name, weight) {
+    this.name = name;
+    this.weight = weight;
+  }
+
+  getWeight() {
+    return this.weight;
+  }
+}
+
+class Container {
+  constructor(maxWeight) {
+    this.maxWeight = maxWeight;
+    this.weight = 0;
+    this.items = [];
+    this.itemType = null;
+  }
+
+  add(item) {
+    if (item.constructor.name !== this.itemType)
+      throw new Error("Wrong kind of object, not allowed!");
+
+    if (this.items.includes(item))
+      throw new Error(`${this.itemType} already added, not allowed!`);
+
+    if (this.getWeight() + item.getWeight() > this.maxWeight)
+      throw new Error("Too heavy, not allowed!");
+    
+    this.items.push(item);
+    return this;
+  }
+
+  getWeight() {
+    this.weight = 0;
+    this.items.forEach(item => this.weight += item.getWeight());
+    return this.weight;
+  }
+}
+
+class Bag extends Container {
+  constructor(maxWeight) {
+    super(maxWeight);
+    this.itemType = "Stuff";
+  }
+}
+
+class Cargo extends Container {
+  constructor(maxWeight) {
+    super(maxWeight);
+    this.itemType = "Bag";
+  }
+}
+
 // koodi:
 var stone = new Stuff("stone", 3);
 var book = new Stuff("book", 7);
@@ -8,27 +62,49 @@ var vuitton = new Bag(3);
 
 var schenker = new Cargo(15);
 
+describe("Stuffs are able to be added to bag", function () {
+  it("Add a stone, bag's weight should be 3", function () {    
+    expect(bag.add(stone).getWeight()).toBe(3);
+  });
 
-bag.add(stone);
-console.log("Bag's weight should be 3: " + bag.weight());
-bag.add(stone); // Error: "Stuff already added, not allowed!"
+  it("Stone is not able to be added again", function () {
+    expect(function () {
+      bag.add(stone)
+    }).toThrowError("Stuff already added, not allowed!");
+  });
 
-bag.add(book);
-console.log("Bag's weight should be 10: " + bag.weight());
+  it("Add a book, bag's weight should be 10", function () {
+    expect(bag.add(book).getWeight()).toBe(10);
+  });
 
-bag.add(cotton); // Error: "Too heavy, not allowed!"
+  it("Cannot add too much stuffs in a bag (no more than max weight)", function () {
+    expect(function () {
+      bag.add(cotton)
+    }).toThrowError("Too heavy, not allowed!");
+  });
 
-console.log("Bag's weight should be 10: " + bag.weight());
+  it("After all, bag's weight should be 10", function () {
+    expect(bag.getWeight()).toBe(10);
+  });
+});
 
+describe("Bags are able to be added to cargo", function () {
+  it("Add bag to cargo, cargo's weight should be 10", function () {
+    expect(schenker.add(bag).getWeight()).toBe(10);
+  });
 
-schenker.add(bag);
-schenker.add(cotton); // Error: Wrong kind of object, not allowed!
+  it("Cannot add cotton to cargo: 'Wrong kind of object, not allowed!'", function () {
+    expect(function () {
+      schenker.add(cotton)
+    }).toThrowError("Wrong kind of object, not allowed!");
+  });
 
-console.log("Cargo's weight should be 10: " + schenker.weight());
+  it("Add cotton to vuitton, add vuitton to cargo, cargo's weight should be about 10.001", function () {
+    expect(schenker.add(vuitton.add(cotton)).getWeight()).toBe(10.001);
+  });
 
-vuitton.add(cotton);
-schenker.add(vuitton);
-console.log("Cargo's weight should be about 10.001: " + schenker.weight()); 
-
-cotton.weight = 300;
-console.log("Cargo's weight should be 310: " + schenker.weight()); // oops!
+  it("Set cotton weight to 300, cargo's weight should be 310", function () {
+    cotton.weight = 300;    
+    expect(schenker.getWeight()).toBe(310);
+  });
+});

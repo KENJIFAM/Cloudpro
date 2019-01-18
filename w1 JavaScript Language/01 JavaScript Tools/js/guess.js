db82acc5076fb54ea40c9388e1205e82aa6f3948
Khoa Pham <kenjifam@gmail.com>
2019-01-18 05:06:19 +0200

    finished exercise w1/01/TODO 2

--- w1 JavaScript Language/01 JavaScript Tools/js/guess.js
+++ w1 JavaScript Language/01 JavaScript Tools/js/guess.js
@@ -1,23 +1,41 @@
 let targetNumber = Math.floor(Math.random() * 10) + 1;
+let stop = false;
+let count = 0;
 
 function init () {
-	console.log("You have to code this!");
+	const number = document.querySelector("input[name='number']");
+	const btn = document.querySelector("button");
+	
+	btn.addEventListener('click', e => {
+		e.preventDefault();
+		if (!stop) check(number.value);
+	});
 }
 
 function check (value) {
-  
+	count++;
+	
+	if (value == targetNumber) {
+		showWin();
+	} else if (count >= 5) {
+		showLoss();
+	} else {
+		showError();
+	}
 }
 
 function showWin () {
-  
+	console.log('Win!');
+	stop = true;
 }
 
 function showError () {
-  
+	console.log('Incorrect!');
 }
 
 function showLoss () {
-  
+	console.log(`Game over! The correct answer is ${targetNumber}`);
+	stop = true;
 }
 
 init();

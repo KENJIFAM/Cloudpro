7f6c873eea107541d7403cc615c35a94630be4d6
Khoa Pham <kenjifam@gmail.com>
2019-01-18 04:56:55 +0200

    finished exercise w1/01/TODO 1

--- w1 JavaScript Language/01 JavaScript Tools/js/calculations.js
+++ w1 JavaScript Language/01 JavaScript Tools/js/calculations.js
@@ -1,12 +1,47 @@
-function multiplyBy()
-{
-// check the input
-    
+const form = document.getElementById("form");
+const result = document.getElementById("result");
+const firstErr = document.getElementById("firstErr");
+const secondErr = document.getElementById("secondErr");
+const firstNumber = document.getElementById("firstNumber");
+const secondNumber = document.getElementById("secondNumber");
+
+const validate = () => {
+  let valid = true;
+
+  if (!firstNumber.value || !secondNumber.value) valid = false;
+
+  if (isNaN(firstNumber.value)) {
+    firstErr.innerText = "Please input a number!";
+    valid = false;
+  } else firstErr.innerText = "";
+  
+  if (isNaN(secondNumber.value)) {
+    secondErr.innerText = "Please input a number!";
+    valid = false;
+  } else secondErr.innerText = "";
+  
+  return valid;
 }
 
-function divideBy() 
-{ 
-// check the input
- 
+function multiplyBy() {
+  if (validate()) {
+    result.innerText = firstNumber.value * secondNumber.value;
+  } else {
+    result.innerText = "";
+  }
 }
 
+function divideBy() { 
+  if (validate()) {
+    if (secondNumber.value != 0) {
+      result.innerText = firstNumber.value / secondNumber.value;
+    } else {
+      secondErr.innerText = "Cannot divide by zero!";
+      result.innerText = "";
+    }
+  } else {
+    result.innerText = "";
+  }
+}
+
+window.onload = () => form.oninput = validate;

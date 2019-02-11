0e514a21af5c9c65c17393dfa34313c658c6e3c8
Khoa Pham <kenjifam@gmail.com>
2019-02-11 03:08:41 +0200

    finished exercise w4/02/TODO 1

--- w4 Vue routing & Node Intro/02 Node Introduction/todo.js
+++ w4 Vue routing & Node Intro/02 Node Introduction/todo.js
@@ -5,12 +5,17 @@ const readline = require('readline-sync')
 
 /* Here we declare an empty array which will be used to store the todo list items. */
 var items = []
+// const items = [] // it still works because we manipulate the array, not reassign it
 
 /* the do-while loop has its exit condition at the end which means it always executes at least once. */
 do {
   /* We capture the text input by the user. This is then converted into a String and finally any _whitespace_ (newline characters, spaces, etc) are removed. */
     // try switching for a let... or a const...
 	var input = String(readline.question('enter command: ')) 
+  // let input = String(readline.question('enter command: ')) : do not work, input is not defined in while loop
+  // const input = String(readline.question('enter command: '))  : do not work, input is not defined in while loop
+
+
   /* The indexOf() function looks for the first ocurrance of the supplied string
   parameter or -1 if it is not found. Notice the use of === to compare the two values,
   this is a 'strict' comparison and should always be used instead of ==. It checks for
@@ -24,6 +29,8 @@ do {
 		console.log('adding "'+item+'"')
     /* All arrays have a built-in push() function which appends an item to their end. */
 		items.push(item)
+    // items.unshift(item) : add the item to the beginning of the array
+    
 	}
 	if (input.indexOf('list') === 0) {
     /* Here we use a for...next loop to interate through all the array indexes. The let keyword defines a variable with _block_ scope (more on this later). */

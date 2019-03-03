23122653404d2319ce4746be17dbb63b138bae90
Khoa Pham <kenjifam@gmail.com>
2019-03-03 16:12:05 +0200

    finished exercise w5/01/TODO 1

--- w5 NodeJS & Testing/01 NodeJS and ECMA6/promises.js
+++ w5 NodeJS & Testing/01 NodeJS and ECMA6/promises.js
@@ -5,30 +5,37 @@
 const stdin = process.openStdin()
 
 /* in this function the anonymous function inside the promise has been declared using the 'traditional' syntax. */
-function reverse(string) {
-    return new Promise(function(resolve, reject) {
+const reverse = string => {
+    return new Promise((resolve, reject) => {
         /* notice the use of the 'const' keyword instead of the usual 'var'. This indicates a constant or 'immutable variable'. */
         const reversed = string.split('').reverse().join('')
-        //reject('could not reverse the string')
         resolve(reversed)
+        reject('could not reverse the string')
     })
 }
 
 /* In this function the anonymous function is declared using the new ECMA6 'arrow' syntax. */
-function capitalise(string) {
+const capitalise= string => {
     return new Promise((resolve, reject) => {
         /* the 'let' keyword means the scope of the following variable is limited to the current code block rather than being scoped by the function. Use of this required strict mode to be implemented. */
         let cap = string.charAt(0).toUpperCase() + string.slice(1)
-        //reject('could not capitalise the string')
         resolve(cap)
+        reject('could not capitalise the string')
     })
 }
+
+const capitaliseLast = string => reverse(string)
+    .then(capitalise)
+    .then(reverse)
+    .catch(err => console.log('could not capitalise the last letter of string'))
 
 /* this promise chain uses the 'traditional' syntax to declare anonymous functions. Notice that the is quite verbose. */
 function promise1(data) {
   reverse(data).then(function(data) {
     /* capitalise() returns a new promise. */
     return capitalise(data)
+  }).then(function(data) {
+    return capitaliseLast(data)
   }).then(function(data) {
     console.log(data)
   }).catch(function(err) {
@@ -41,6 +48,8 @@ function promise2(data) {
   reverse(data).then(data => {
     return capitalise(data)
   }).then(data => {
+    return capitaliseLast(data)
+  }).then(data => {
     console.log(data)
   }).catch((err) => {
     console.log('an error occurred: '+err)
@@ -51,6 +60,7 @@ function promise2(data) {
 function promise3(data) {
   reverse(data)
     .then( data => capitalise(data)  )
+    .then( data => capitaliseLast(data) )
     .then( data => console.log(data) )
     .catch( (e) => console.log('an error occurred: '+e) )
 }
@@ -59,6 +69,18 @@ function promise3(data) {
 function promise4(data) {
   reverse(data)
     .then(capitalise)
+    .then(capitaliseLast)
+    .then( data => console.log(data) )
+    .catch(  (e) => console.log('an error occurred: '+e) )
+}
+
+/* Question 4: Achieve the same result without needing a third function */
+function promise5(data) {
+  reverse(data)
+    .then(capitalise)
+    .then(reverse)
+    .then(capitalise)
+    .then(reverse)
     .then( data => console.log(data) )
     .catch(  (e) => console.log('an error occurred: '+e) )
 }
@@ -70,4 +92,5 @@ stdin.on('data', function(chunk) {
   promise2(text)
   promise3(text)
   promise4(text)
+  promise5(text)
 })
